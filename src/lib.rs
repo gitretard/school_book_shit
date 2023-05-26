da091b0ca97f3ea588f019ac6a67743c7be38620
gitretard <znoobbitcatspro@gmail.com>
2023-05-26 20:30:32 +0700

    uhhh it works? kinda. also switched to structs instead of plain text

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
 use std::{
     fmt, fs,
-    io::{BufRead, BufReader, Read, Write,Seek,SeekFrom},
-    path, process,
+    io::{BufRead, BufReader, Read, Write},
+    path, process, time
 };
+
+pub struct borrow<>{
+    borrow_when: time::Instant,
+    return_after: time::Duration,
+    borrow_names: Vec<u32>,
+}
+pub struct member{
+    pub name: String,
+    pub mid: u32,
+    pub borrow: Option<borrow>,
+}
 
 pub trait pretty_unwrap<T, E> {
     fn pretty_unwrap(self, msg: Option<&str>) -> T;
@@ -45,40 +56,41 @@ where
 }
 // This is Steve. He manages the record file and do stuff
 pub struct steve {
-    pub uidcount: u32,
-    pub records: Vec<String>,
+    pub midcount: u32,
+    pub records: Vec<member>,
 }
 impl steve {
     pub fn sync(&mut self) {
-        let mut ubuf: [u8; 4] = [0; 4];
-        let mut file = fs::File::options()
+        let recfile = fs::File::options()
             .read(true)
             .open("rec")
             .pretty_unwrap(Some("Cannot open rec"));
-        file.read_exact(&mut ubuf)
-            .pretty_unwrap(Some("Cant read form rec. 4 bytes"));
-        self.uidcount = u32::from_be_bytes(ubuf);
+        let mut midcf = fs::File::open("midcf").unwrap();
+        let mut midcbuf:[u8;4] = [0;4];
+        midcf.read_exact(&mut midcbuf).pretty_unwrap(Some("cant read 4 bytes from midcf"));
         // Load every record name into memory (trust me bro it wont be that resource intensive and slow)
-        let reader = BufReader::new(file);
+        let reader = BufReader::new(recfile);
+        let mut tmpmcount:u32 = 0;
         for i in reader.lines() {
-            let i: String = i.pretty_unwrap(Some("Shit failed somehow"));
-            self.records.insert(self.records.len(), i);
+            let i = i.pretty_unwrap(Some("rj809j9wg9j0"));
+            let i:Vec<&str> = i.split("::").collect();
+            let s:member = member { name: i[0].to_string(), mid:tmpmcount, borrow:None };
+            self.records.insert(self.records.len(),s);
+            tmpmcount+=1
         }
+        self.midcount = tmpmcount
     }
     // Create new record (overwrites existing ones)
     pub fn init_record(&self) {
-        let mut file =
-            fs::File::create("rec").pretty_unwrap(Some("steve: failed to open rec as rw"));
+        fs::File::create("rec").pretty_unwrap(Some("steve: failed to open rec as rw"));
         // init 0 u32
-        file.write_all(&[0; 4])
-            .pretty_unwrap(Some("steve: cannot write 4 null bytes to new rec"));
-        file.write_all(&[10; 1])
-            .pretty_unwrap(Some("steve: cant write LF to new rec"));
+        let mut midcf = fs::File::create("midcf").pretty_unwrap(Some("Failed to create midc"));
+        midcf.write(&[0;4]).unwrap();
     }
     pub fn record_register(&mut self, name: &str) {
         // cock
         for i in self.records.iter() {
-            let i = i.split("::").collect::<Vec<&str>>()[0];
+            let i = i.name.split("::").collect::<Vec<&str>>()[0];
             if name == i{
                 println!("steve: Cancelling register due to repeating names {{{name}}}");
                 return;
@@ -88,17 +100,18 @@ impl steve {
         if !p.exists() {
             panic!("steve: cannot record register! rec not found")
         }
-        self.records.insert(self.records.len(),format!("{}::{}",name,self.uidcount));
+        self.records.insert(self.records.len(),member{name: format!("{}::{}",name,self.midcount),mid:self.midcount,borrow:None});
         let mut file = fs::File::options()
             .append(true)
             .read(true)
             .write(true)
             .open("rec")
             .pretty_unwrap(Some("steve: cannot open rec for register"));
-        file.write(format!("{}::{}\n", name, self.uidcount).as_bytes())
+        file.write(format!("{}::{}\n", name, self.midcount).as_bytes())
             .pretty_unwrap(Some("steve: cannot write to rec for register"));
-        self.uidcount += 1;
-        // wish there was a proper way to MODIFY FUCKING SPECIFIC BYTES
+        let mut midcf = fs::File::options().write(true).open("midcf").pretty_unwrap(Some("Failed to open midcf as write-only"));
+        self.midcount += 1;
+        midcf.write(&self.midcount.to_be_bytes()).pretty_unwrap(Some("failed to write to midcf record_register"));
         
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[allow(dead_code)]
 mod lib;
 fn main(){
-    let mut steve = lib::steve{uidcount:0,records:Vec::new()};
+    let mut steve = lib::steve{midcount:0,records:Vec::new()};
     let path = std::path::Path::new("rec");
     if !path.exists(){
         steve.init_record()
     }
     steve.sync();
-    steve.record_register("bob");
-    steve.record_register("mike");
-    steve.record_register("john");
-    steve.record_register("joe");
-    
+    steve.record_register("ben");
 }
